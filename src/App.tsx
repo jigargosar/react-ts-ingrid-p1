aa87d4cdf91cee91bcf09875f666fac5a3b0a8e2
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-18 17:04:29 +0530

    add mobx

--- src/App.tsx
+++ src/App.tsx
@@ -45,14 +45,19 @@ const DnDList = observer(
     list: any[]
     renderItem: (item: any, idx: number) => React.ReactNode
   }) => {
-    const state = useObservable({ di: null })
+    const state = useObservable({ di: null, px: 0, py: 0 })
 
     useEffect(() => {
       function mouseUpListener(e: MouseEvent) {
         state.di = null
       }
 
-      function mouseMoveListener(e: MouseEvent) {}
+      function mouseMoveListener(e: MouseEvent) {
+        if (state.di) {
+          state.px = e.pageX
+          state.py = e.pageY
+        }
+      }
 
       window.addEventListener('mouseup', mouseUpListener)
       window.addEventListener('mousemove', mouseMoveListener)
@@ -62,21 +67,31 @@ const DnDList = observer(
       }
     }, [])
 
+    const isDragging = !!state.di
     return (
-      <div className="relative">
+      <div className={cn('relative', { 'us-none': isDragging })}>
         {list.map((item, idx) => {
           return (
             <div
-              className={cn({ 'o-50': item === state.di })}
-              onMouseDown={() => (state.di = item)}
+              className={cn({ 'o-30': item === state.di })}
+              onMouseDown={e => {
+                e.persist()
+                console.log(`e`, e)
+                state.di = item
+                state.px = e.pageX
+                state.py = e.pageY
+              }}
               key={idx}
             >
               {renderItem(item, idx)}
             </div>
           )
         })}
-        {state.di && (
-          <div className="absolute" style={{ top: 10, left: 10 }}>
+        {isDragging && (
+          <div
+            className="absolute"
+            style={{ left: state.px, top: state.py }}
+          >
             {renderItem(state.di, -1)}
           </div>
         )}
