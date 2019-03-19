05392f77de21f4b4ee63f4c3b0fab79633c3af39
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-19 13:26:04 +0530

    storybook storysource

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
 import { configure } from '@storybook/react'
+import { configureActions } from '@storybook/addon-actions'
+
+configureActions({ depth: 10, clearOnStoryChange: true, limit: 50 })
 
 function loadStories() {
   require('../src/stories/index.stories')
