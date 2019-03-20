ce70e8900c965cdd3333a9b90fd6f1bdbbb8438e
Jigar Gosar <jigar.gosar@gmail.com>
2019-03-20 11:27:45 +0530

    extract NodeCollection

--- src/Store.tsx
+++ src/Store.tsx
@@ -1,88 +1,12 @@
 import { useEffect, useState } from 'react'
-import { action, autorun, observable, values } from 'mobx'
+import { action, autorun, observable } from 'mobx'
 import isHotkey from 'is-hotkey'
 import { getCached, setCache } from './cache-helpers'
 import { useDisposable } from 'mobx-react-lite'
 import { NodeModel, NodeModelJSON } from './model/NodeModel'
+import { NodeCollection } from './model/NodeCollection'
 
 // configure({ enforceActions: 'always', computedRequiresReaction: true })
-
-class NodeCollection {
-  @observable byId: { [index: string]: NodeModel }
-  constructor(byId: { [index: string]: NodeModel }) {
-    this.byId = byId
-  }
-
-  toJSON() {
-    return Object.values(this.byId).map(node => node.toJSON())
-  }
-
-  static fromJSON(json: NodeModelJSON[]) {
-    const byId = json.reduce(
-      (
-        acc: { [index: string]: NodeModel },
-        { _id, title, childIds, collapsed },
-      ) => {
-        acc[_id] = NodeModel.createNew(_id, title, childIds, collapsed)
-        return acc
-      },
-      {},
-    )
-    return new NodeCollection(byId)
-  }
-
-  registerNode(node: NodeModel) {
-    this.byId[node.id] = node
-  }
-
-  public getVisibleChildrenOf(node: NodeModel) {
-    return node.hasVisibleChildren ? this.getChildNodesOf(node) : []
-  }
-
-  private getChildNodesOf(node: NodeModel) {
-    return node.childIds.map(childNode => this.maybeNodeWithId(childNode))
-  }
-
-  maybeNodeWithId(id: string) {
-    return this.byId[id]
-  }
-
-  public get rootNode() {
-    return this.byId[NodeModel.rootNodeId]
-  }
-
-  private get idToPidLookup() {
-    return values(this.byId).reduce((acc, node) => {
-      node.childIds.forEach((cid: string) => {
-        acc[cid] = node.id
-      })
-      return acc
-    }, {})
-  }
-
-  maybeParentIdOfId(nodeId: string) {
-    return this.idToPidLookup[nodeId]
-  }
-
-  maybeParentIdOf(node: NodeModel) {
-    return this.idToPidLookup[node.id]
-  }
-
-  private maybeParentOf(node: NodeModel) {
-    const pid = this.maybeParentIdOf(node)
-    return pid && this.maybeNodeWithId(pid)
-  }
-
-  private isRootNode(node: NodeModel) {
-    return this.rootNode === node
-  }
-
-  static create() {
-    const nodeCollection = new NodeCollection({})
-    nodeCollection.registerNode(NodeModel.getOrCreateRootNode())
-    return nodeCollection
-  }
-}
 
 export class Store {
   @observable nodeCollection: NodeCollection
---
+++ src/model/NodeCollection.tsx
@@ -0,0 +1,80 @@
+import { observable, values } from 'mobx'
+import { NodeModel, NodeModelJSON } from './NodeModel'
+
+export class NodeCollection {
+  @observable byId: { [index: string]: NodeModel }
+
+  constructor(byId: { [index: string]: NodeModel }) {
+    this.byId = byId
+  }
+
+  toJSON() {
+    return Object.values(this.byId).map(node => node.toJSON())
+  }
+
+  static fromJSON(json: NodeModelJSON[]) {
+    const byId = json.reduce(
+      (
+        acc: { [index: string]: NodeModel },
+        { _id, title, childIds, collapsed },
+      ) => {
+        acc[_id] = NodeModel.createNew(_id, title, childIds, collapsed)
+        return acc
+      },
+      {},
+    )
+    return new NodeCollection(byId)
+  }
+
+  registerNode(node: NodeModel) {
+    this.byId[node.id] = node
+  }
+
+  public getVisibleChildrenOf(node: NodeModel) {
+    return node.hasVisibleChildren ? this.getChildNodesOf(node) : []
+  }
+
+  private getChildNodesOf(node: NodeModel) {
+    return node.childIds.map(childNode => this.maybeNodeWithId(childNode))
+  }
+
+  maybeNodeWithId(id: string) {
+    return this.byId[id]
+  }
+
+  public get rootNode() {
+    return this.byId[NodeModel.rootNodeId]
+  }
+
+  private get idToPidLookup() {
+    return values(this.byId).reduce((acc, node) => {
+      node.childIds.forEach((cid: string) => {
+        acc[cid] = node.id
+      })
+      return acc
+    }, {})
+  }
+
+  maybeParentIdOfId(nodeId: string) {
+    return this.idToPidLookup[nodeId]
+  }
+
+  maybeParentIdOf(node: NodeModel) {
+    return this.idToPidLookup[node.id]
+  }
+
+  private maybeParentOf(node: NodeModel) {
+    const pid = this.maybeParentIdOf(node)
+    return pid && this.maybeNodeWithId(pid)
+  }
+
+  private isRootNode(node: NodeModel) {
+    return this.rootNode === node
+  }
+
+  static create() {
+    const nodeCollection = new NodeCollection({})
+    nodeCollection.registerNode(NodeModel.getOrCreateRootNode())
+    return nodeCollection
+  }
+}
